4d782d256fa98f0b29b677588f4a332a80fd6696
aroca.twelve@proton.me <aroca.twelve@proton.me>
2024-10-21 19:13:38 -0400

    added project 1

---
+++ 1-BlockLetters/initials.py
@@ -0,0 +1,15 @@
+# my name is nico aroca
+
+# i like coding
+
+initials = """
+N     N    A
+NN    N   A A
+N N   N  A   A
+N  N  N  AAAAA
+N   N N  A   A
+N    NN  A   A
+N     N  A   A
+"""
+
+print(initials)
